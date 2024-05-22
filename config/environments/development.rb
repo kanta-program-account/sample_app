ee87f4ffab294858a8cce3af9cf529ddb91eb062
kanta-program-account <151516153+kanta-program-account@users.noreply.github.com>
2024-05-22 13:29:00 +0000

    [break]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "001ffa8957bf4957b17fed2b9e22d9ab.vfs.cloud9.us-east-1.amazonaws.com"
 end
