4fd9576f19f2ceddc17988fbee66404287915e2f
kanta-program-account <151516153+kanta-program-account@users.noreply.github.com>
2024-05-24 08:04:43 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   
   get 'lists' => 'lists#index'
   
+  get 'lists/:id' => 'lists#show' ,as: 'list'
   
-  get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
