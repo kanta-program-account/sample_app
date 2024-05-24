07070eeeae9a9d87e0070ded1c4db54fd39478b7
kanta-program-account <151516153+kanta-program-account@users.noreply.github.com>
2024-05-24 07:36:49 +0000

    [Finish] 3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+  get 'homes' => 'homes#top'
+  
+  get 'lists/new' => 'lists#new'
   post 'lists' => 'lists#create'
   
-  get 'lists/index'
+  get 'lists' => 'lists#index'
+  
+  
   get 'lists/show'
   get 'lists/edit'
   get '/top' => 'homes#top'
